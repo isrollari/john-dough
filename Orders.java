79f43e2fc35a046fe86562f0c4b3f2d2c1f0b7f4
qwis95 <44578739+qwis95@users.noreply.github.com>
2023-04-18 16:53:53 -0600

    Update Orders.java
    Orders is working with a sample print structure

--- Orders.java
+++ Orders.java
@@ -1,6 +1,67 @@
 package johnDough;
 
-public class Orders {
-	 //TODO: generate an ArrayList<Order>, there shouldn't be a need to add/remove orders. just let it empty when process is terminated
+import java.util.ArrayList;
 
+public class Orders {
+	 //TODO:
+	ArrayList<Order> inventory = new ArrayList<>();
+	int i=1;
+	public void placeOrder(String date, String name, int glazed, int sugarR, int chocolateR, int plain,
+			int chocolateC, int sugarC, int lemon, int grape, int custard) 
+	{
+		Order temp = new Order(i, date, name, glazed,  sugarR,  chocolateR,  plain,
+				 chocolateC,  sugarC,  lemon,  grape,  custard);
+		inventory.add(temp);
+		this.i++;
+	}
+	
+	public void printOrder(int index)
+	{
+		System.out.println("Date: "+inventory.get(index).getDate());
+		System.out.println("Name: "+inventory.get(index).getName());
+		System.out.println("Total Quantity: "+inventory.get(index).getTotal_quantity());
+		System.out.println("Price: "+inventory.get(index).getTotal_price());
+		System.out.println("Order ID "+inventory.get(index).getOrder_ID());
+		if(inventory.get(index).getGlazed() != 0) {
+			System.out.println("Number of Glazed Doughnuts: "+ inventory.get(index).getGlazed());
+		}
+		if(inventory.get(index).getSugarR() != 0) {
+			System.out.println("Number of Raised Sugar Doughnuts: "+ inventory.get(index).getSugarR());
+		}
+		if(inventory.get(index).getChocolateR() != 0) {
+			System.out.println("Number of Raised Chocolate Doughnuts: "+ inventory.get(index).getChocolateR());
+		}
+		if(inventory.get(index).getPlain() != 0) {
+			System.out.println("Number of Plain Cake Doughnuts: "+ inventory.get(index).getPlain());
+		}
+		if(inventory.get(index).getChocolateC() != 0) {
+			System.out.println("Number of Chocolate Cake Doughnuts: "+ inventory.get(index).getChocolateC());
+		}
+		if(inventory.get(index).getSugarC() != 0) {
+			System.out.println("Number of Sugar Cake Doughnuts: "+ inventory.get(index).getSugarC());
+		}
+		if(inventory.get(index).getLemon() != 0) {
+			System.out.println("Number of Lemon Filled Doughnuts: "+ inventory.get(index).getLemon());
+		}
+		if(inventory.get(index).getGrape() != 0) {
+			System.out.println("Number of Grape Filled Doughnuts: "+ inventory.get(index).getGrape());
+		}
+		if(inventory.get(index).getCustard() != 0) {
+			System.out.println("Number of Custard Filled Doughnuts: "+ inventory.get(index).getCustard());
+		}
+		
+		
+		
+		
+		
+		/*
+		if(inventory.get(index).isIs_processed())
+		{
+			System.out.println("Order has been processed");
+		}else {
+			System.out.println("Order has not been processed");
+		}
+		*/
+	}
 }
+
