6773172626f1742db49ba9dd90d4790274f3e0c9
BlakeBeladona <orionforestjoseph@gmail.com>
2023-04-18 17:53:41 -0600

    Update Tray.java

--- Tray.java
+++ Tray.java
@@ -1,16 +1,43 @@
 package johnDough;
+import java.time.*;
 
 public class Tray {
-String bake_time;
-String days_old;
-String mType;
-String sType;
+	LocalTime baketime;
+	int daysold;
+	String mainType;
+	String subType;
+	int trayID;
+	int numAvailable;
 
-int tray_ID;
-int num_available;
-
-
-
-
-
+	public Tray(String mType, String sType, int ID) {
+		baketime = LocalTime.now();
+		daysold = 0;
+		mainType = mType;
+		subType = sType;
+		trayID = ID;
+		numAvailable = 200;
+	}
+	
+	public Tray(String mType, String sType, int ID, LocalTime bTime, int dOld, int curDonuts) {
+		baketime = bTime;
+		daysold = dOld;
+		mainType = mType;
+		subType = sType;
+		trayID = ID;
+		numAvailable = curDonuts;
+	}
+	
+	public int removeDonuts(int num) {
+		if (num > numAvailable) {
+			numAvailable = 0;
+			return num - numAvailable;
+		} else {
+			numAvailable -= num;
+			return 0;
+		}
+	}
+	
+	public void updateDaysOld() {
+		daysold++;
+	}
 }
