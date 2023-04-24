c9e718aa7aba9f669391499ca41fcc23d84fc767
BlakeBeladona <orionforestjoseph@gmail.com>
2023-04-24 16:21:44 -0600

    Update AdminOptions.java

--- AdminOptions.java
+++ AdminOptions.java
@@ -1,5 +1,12 @@
 package johnDough;
 
-public class AdminOptions {
+public class AdminOptions extends Menu {
 	// TODO I think this file is better than trying to cram the Admin functions into main
+	public AdminOptions () {
+	
+	}
+	
+	public void run() {
+		
+	}
 }
