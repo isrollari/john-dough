6d3809a4d60649c3861e7d84e397dbdc7dc0c1e4
isrollari <71795089+isrollari@users.noreply.github.com>
2023-04-24 20:07:43 -0600

    Add files via upload

--- adminReport.java
+++ adminReport.java
@@ -3,29 +3,46 @@ package johnDough;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class adminReport {
 
     private String reportTime;
     private int totalRaised;
     private int glazed;
+    private float priceGlazed;
     private int sugarR;
+    private float priceSugarR;
     private int chocolateR;
+    private float priceChocolateR;
     private int totalCake;
     private int plain;
+    private float pricePlain;
     private int chocolateC;
+    private float priceChocolateC;
     private int sugarC;
+    private float priceSugarC;
     private int totalFilled;
     private int lemon;
+    private float priceLemon;
     private int grape;
+    private float priceGrape;
     private int custard;
-    private int totalPrice;
+    private float priceCustard;
+    private float totalPrice;
     private int totalQuantity;
     private int staleDoughnts;
     public static ArrayList<adminReport> adminReportData = new ArrayList<>();
     public static ArrayList<adminReport> adminReportStaleData = new ArrayList<>();
 
     public adminReport(){
+
+    }
+
+    public static void openAdminReport(){
         String csvFile = "admin-report.csv";
         String line = "";
         String csvSplitBy = ",";
@@ -46,33 +63,51 @@ public class adminReport {
                 String reportTime = (orderStr[0]);
                 int totalRaised = Integer.parseInt(orderStr[1]);
                 int glazed = Integer.parseInt(orderStr[2]);
-                int sugarR = Integer.parseInt(orderStr[3]);
-                int chocolateR = Integer.parseInt(orderStr[4]);
-                int totalCake = Integer.parseInt(orderStr[5]);
-                int plain = Integer.parseInt(orderStr[6]);
-                int chocolateC = Integer.parseInt(orderStr[7]);
-                int sugarC = Integer.parseInt(orderStr[8]);
-                int totalFilled = Integer.parseInt(orderStr[9]);
-                int lemon = Integer.parseInt(orderStr[10]);
-                int grape = Integer.parseInt(orderStr[11]);
-                int custard = Integer.parseInt(orderStr[12]);
-                int totalPrice = Integer.parseInt(orderStr[13]);
-                int totalQuantity = Integer.parseInt(orderStr[14]);
+                float glazedPrice = Float.parseFloat(orderStr[3]);
+                int sugarR = Integer.parseInt(orderStr[4]);
+                float sugarRPrice = Float.parseFloat(orderStr[5]);
+                int chocolateR = Integer.parseInt(orderStr[6]);
+                float chocolateRPrice = Float.parseFloat(orderStr[7]);
+                int totalCake = Integer.parseInt(orderStr[8]);
+                int plain = Integer.parseInt(orderStr[9]);
+                float plainPrice = Float.parseFloat(orderStr[10]);
+                int chocolateC = Integer.parseInt(orderStr[11]);
+                float chocolateCPrice = Float.parseFloat(orderStr[12]);
+                int sugarC = Integer.parseInt(orderStr[13]);
+                float sugarCPrice = Float.parseFloat(orderStr[14]);
+                int totalFilled = Integer.parseInt(orderStr[15]);
+                int lemon = Integer.parseInt(orderStr[16]);
+                float lemonPrice = Float.parseFloat(orderStr[17]);
+                int grape = Integer.parseInt(orderStr[18]);
+                float grapePrice = Float.parseFloat(orderStr[19]);
+                int custard = Integer.parseInt(orderStr[20]);
+                float custardPrice = Float.parseFloat(orderStr[21]);
+                float totalPrice = Float.parseFloat(orderStr[22]);
+                int totalQuantity = Integer.parseInt(orderStr[23]);
 
                 adminReport adminreport = new adminReport();
                 adminreport.setReportTime(reportTime);
                 adminreport.setTotalRaised(totalRaised);
                 adminreport.setGlazed(glazed);
+                adminreport.setPriceGlazed(glazedPrice);
                 adminreport.setSugarR(sugarR);
+                adminreport.setPriceSugarR(sugarRPrice);
                 adminreport.setChocolateR(chocolateR);
+                adminreport.setPriceChocolateR(chocolateRPrice);
                 adminreport.setTotalCake(totalCake);
                 adminreport.setPlain(plain);
+                adminreport.setPricePlain(plainPrice);
                 adminreport.setChocolateC(chocolateC);
+                adminreport.setPriceChocolateC(chocolateCPrice);
                 adminreport.setSugarC(sugarC);
+                adminreport.setPriceSugarC(sugarCPrice);
                 adminreport.setTotalFilled(totalFilled);
                 adminreport.setLemon(lemon);
+                adminreport.setPriceLemon(lemonPrice);
                 adminreport.setGrape(grape);
+                adminreport.setPriceGrape(grapePrice);
                 adminreport.setCustard(custard);
+                adminreport.setPriceCustard(custardPrice);
                 adminreport.setTotalPrice(totalPrice);
                 adminreport.setTotalQuantity(totalQuantity);
 
@@ -84,7 +119,7 @@ public class adminReport {
         }
     }
 
-    public static void adminReportStale(){
+    public static void openAdminReportStale(){
         String csvFile = "admin-report-stale.csv";
         String line = "";
         String csvSplitBy = ",";
@@ -106,6 +141,7 @@ public class adminReport {
                 int stale = Integer.parseInt(orderStr[1]);
 
                 adminReport adminreport = new adminReport();
+                adminreport.setReportTime(reportTime);
                 adminreport.setStaleDoughnts(stale);
 
                 adminReportStaleData.add(adminreport);
@@ -130,25 +166,43 @@ public class adminReport {
                 csvWriter.append(",");
                 csvWriter.append((char) adminreport.getGlazed());
                 csvWriter.append(",");
+                csvWriter.append((char) adminreport.getPriceGlazed());
+                csvWriter.append(",");
                 csvWriter.append((char) adminreport.getSugarR());
                 csvWriter.append(",");
+                csvWriter.append((char) adminreport.getPriceSugarR());
+                csvWriter.append(",");
                 csvWriter.append((char) adminreport.getChocolateR());
+                csvWriter.append(",");
+                csvWriter.append((char) adminreport.getPriceChocolateR());
                 csvWriter.append(",");
                 csvWriter.append((char) adminreport.getTotalCake());
                 csvWriter.append(",");
                 csvWriter.append((char) adminreport.getPlain());
                 csvWriter.append(",");
+                csvWriter.append((char) adminreport.getPricePlain());
+                csvWriter.append(",");
                 csvWriter.append((char) adminreport.getChocolateC());
                 csvWriter.append(",");
+                csvWriter.append((char) adminreport.getPriceChocolateC());
+                csvWriter.append(",");
                 csvWriter.append((char) adminreport.getSugarC());
+                csvWriter.append(",");
+                csvWriter.append((char) adminreport.getPriceSugarC());
                 csvWriter.append(",");
                 csvWriter.append((char) adminreport.getTotalFilled());
                 csvWriter.append(",");
                 csvWriter.append((char) adminreport.getLemon());
                 csvWriter.append(",");
+                csvWriter.append((char) adminreport.getPriceLemon());
+                csvWriter.append(",");
                 csvWriter.append((char) adminreport.getGrape());
                 csvWriter.append(",");
+                csvWriter.append((char) adminreport.getPriceGrape());
+                csvWriter.append(",");
                 csvWriter.append((char) adminreport.getCustard());
+                csvWriter.append(",");
+                csvWriter.append((char) adminreport.getPriceCustard());
                 csvWriter.append(",");
                 csvWriter.append((char) adminreport.getTotalPrice());
                 csvWriter.append(",");
@@ -166,6 +220,123 @@ public class adminReport {
         } catch (IOException e) {
             System.err.println("Error writing inventory to CSV file: " + e.getMessage());
         }
+    }
+
+    public static void printAdminReport(){
+        for (adminReport adminreport : adminReportData) {
+            System.out.println("Report type: "+ adminreport.getReportTime());
+            System.out.println("totalRaised: "+ adminreport.getTotalRaised());
+            System.out.println("Glazed quantity: " + adminreport.getGlazed());
+            System.out.println("Glazed price: " + adminreport.getPriceGlazed());
+            System.out.println("SugarR quantity: "+ adminreport.getSugarR());
+            System.out.println("SugarR price: "+ adminreport.getPriceSugarR());
+            System.out.println("ChocolateR quantity: " + adminreport.getChocolateR());
+            System.out.println("ChocolateR price: " + adminreport.getPriceChocolateR());
+            System.out.println("totalCake quantity: "+ adminreport.getTotalCake());
+            System.out.println("Plain quantity: " + adminreport.getPlain());
+            System.out.println("Plain price: " + adminreport.getPricePlain());
+            System.out.println("ChocolateC quantity: "+ adminreport.getChocolateC());
+            System.out.println("ChocolateC price: "+ adminreport.getPriceChocolateC());
+            System.out.println("SugarC quantity: " + adminreport.getSugarC());
+            System.out.println("SugarC price: " + adminreport.getPriceSugarC());
+            System.out.println("totalFilled: "+ adminreport.getTotalFilled());
+            System.out.println("Lemon quantity: " + adminreport.getLemon());
+            System.out.println("Lemon price: " + adminreport.getPriceLemon());
+            System.out.println("Grape quantity: "+ adminreport.getGrape());
+            System.out.println("Grape price: "+ adminreport.getGrape());
+            System.out.println("Custard quantity: "+ adminreport.getCustard());
+            System.out.println("Custard price: "+ adminreport.getPriceCustard());
+            System.out.println("Price: "+ adminreport.getTotalPrice());
+            System.out.println("Quantity: "+ adminreport.getTotalQuantity());
+            System.out.println();
+        }
+    }
+
+    public static void printAdminReportStale(){
+        for (adminReport adminreport : adminReportStaleData) {
+            System.out.println("Report type: "+ adminreport.getReportTime());
+            System.out.println("Num of stale: " + adminreport.getStaleDoughnts());
+            System.out.println();
+        }
+    }
+
+    public float getPriceGlazed() {
+        return priceGlazed;
+    }
+
+    public void setPriceGlazed(float priceGlazed) {
+        this.priceGlazed = priceGlazed;
+    }
+
+    public float getPriceSugarR() {
+        return priceSugarR;
+    }
+
+    public void setPriceSugarR(float priceSugarR) {
+        this.priceSugarR = priceSugarR;
+    }
+
+    public float getPriceChocolateR() {
+        return priceChocolateR;
+    }
+
+    public void setPriceChocolateR(float priceChocolateR) {
+        this.priceChocolateR = priceChocolateR;
+    }
+
+    public float getPricePlain() {
+        return pricePlain;
+    }
+
+    public void setPricePlain(float pricePlain) {
+        this.pricePlain = pricePlain;
+    }
+
+    public float getPriceChocolateC() {
+        return priceChocolateC;
+    }
+
+    public void setPriceChocolateC(float priceChocolateC) {
+        this.priceChocolateC = priceChocolateC;
+    }
+
+    public float getPriceSugarC() {
+        return priceSugarC;
+    }
+
+    public void setPriceSugarC(float priceSugarC) {
+        this.priceSugarC = priceSugarC;
+    }
+
+    public float getPriceLemon() {
+        return priceLemon;
+    }
+
+    public void setPriceLemon(float priceLemon) {
+        this.priceLemon = priceLemon;
+    }
+
+    public float getPriceGrape() {
+        return priceGrape;
+    }
+
+    public void setPriceGrape(float priceGrape) {
+        this.priceGrape = priceGrape;
+    }
+
+    public float getPriceCustard() {
+        return priceCustard;
+    }
+
+    public void setPriceCustard(float priceCustard) {
+        this.priceCustard = priceCustard;
+    }
+
+    public float getTotalPrice() {
+        return totalPrice;
+    }
+    public void setTotalPrice(float totalPrice) {
+        this.totalPrice = totalPrice;
     }
 
     public int getStaleDoughnts() {
@@ -280,13 +451,6 @@ public class adminReport {
         this.custard = custard;
     }
 
-    public int getTotalPrice() {
-        return totalPrice;
-    }
-
-    public void setTotalPrice(int totalPrice) {
-        this.totalPrice = totalPrice;
-    }
 
     public int getTotalQuantity() {
         return totalQuantity;
